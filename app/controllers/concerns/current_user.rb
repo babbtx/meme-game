90642c0443de051628af376a8f2303f5eba8eabb
David Babbitt <dbabbitt@yahoo.com>
2020-04-11 20:15:02 -0500

    always load current user

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -6,21 +6,30 @@ module CurrentUser
   end
 
   included do
+    before_action :ensure_current_user
     rescue_from UserNotFound, with: :render_jwt_unauthorized
   end
 
   private
 
   # locates the user identified by the 'sub' attribute of the authz_jwt
+  def current_user
+    @current_user ||= begin
+      authenticate_request!
+      User.find_by(token_subject: @authz_jwt[:sub])
+    end
+  end
+
+  # locates the user identified by the 'sub' attribute of the authz_jwt
   # if that user doesn't exist, it creates the user, and if that fails,
   # raises UserNotFound (which by default renders unauthorized)
   def current_user!
-    @current_user ||= begin
-      authenticate_request!
-      subject = @authz_jwt[:sub]
-      User.find_by(token_subject: subject) || User.create!(token_subject: subject)
-    end
+    @current_user = current_user || User.create!(token_subject: @authz_jwt[:sub])
   rescue ActiveRecord::RecordNotSaved => ex
     raise UserNotFound.new(ex.message)
+  end
+
+  def ensure_current_user
+    !! current_user!
   end
 end
